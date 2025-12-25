f720af561a26e0a87dbbbbf7828deac379de98f0
Daniel Elliott <agile.guy@hotmail.com>
2025-12-24 21:45:33 -0700

    chore: add ESLint configuration with Jest plugin

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+          node: true,
+          es2021: true,
+          jest: true,
+    },
+    extends: [
+          'eslint:recommended',
+          'plugin:jest/recommended',
+          'prettier',
+        ],
+    parserOptions: {
+          ecmaVersion: 'latest',
+          sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+          'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+          'no-console': 'warn',
+          'prefer-const': 'error',
+          'eqeqeq': ['error', 'always'],
+          'curly': ['error', 'all'],
+    },
+};
